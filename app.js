8b9a9888b44bc4bb864cd80518ea7e5185797b72
bouyyah <hamameayyoub@gmail.com>
2020-09-23 05:10:16 +0100

    Generating todo list

--- app.js
+++ app.js
@@ -9,5 +9,27 @@ todoButton.addEventListener('click', addTodo)
 //FUNCTIONS
 
 function addTodo(event){
-    console.log('Heelloo')
+//preventing page from refreshiing
+    event.preventDefault();
+//Create todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+//Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'Hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+//Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+//Trash mark button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+//append div to list
+    todoList.appendChild(todoDiv);
+
 }
