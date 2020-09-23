5a8b861a5b94c81e305fd3bc319b7803d31a5256
bouyyah <hamameayyoub@gmail.com>
2020-09-23 04:45:19 +0100

    Adding some style

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// event listnerr
+//SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//EVENT LISTNERS
+todoButton.addEventListener('click', addTodo)
+
+//FUNCTIONS
+
+function addTodo(event){
+    console.log('Heelloo')
+}
